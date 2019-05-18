c98a6f736ace705c9313e5168f0071bacc496382
Benjamin Dordoigne <benjamin.d@onthegosystems.com>
2019-05-18 07:27:16 +0100

    Added New Files

--- home-page.php
+++ home-page.php
@@ -21,33 +21,18 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 
 <div class="wrapper" id="full-width-page-wrapper">
-
 	<div class="<?php echo esc_attr( $container ); ?>" id="content">
-
 		<div class="row">
-
 			<div class="col-md-12 content-area" id="primary">
-
 				<main class="site-main" id="main" role="main">
-
-                    <?php while ( have_posts() ) : the_post(); ?>
-                    
+                    <?php while ( have_posts() ) : the_post(); ?>                    
                         <?php echo do_shortcode('[wonderplugin_slider id=1]'); ?>
-
                         <?php the_content();?>						
-
-						Will Need to add ACF here
-
+						<h1>We are Seventh Art Productions </h1>
 					<?php endwhile; // end of the loop. ?>
-
 				</main><!-- #main -->
-
 			</div><!-- #primary -->
-
 		</div><!-- .row end -->
-
 	</div><!-- #content -->
-
 </div><!-- #full-width-page-wrapper -->
-
 <?php get_footer(); ?>
