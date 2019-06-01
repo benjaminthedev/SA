408b7d727748e9e76e131a9bcc1cca6734872d3e
Benjamin Dordoigne <benjamin.d@onthegosystems.com>
2019-06-01 07:02:45 +0100

    Porject Update

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,34 @@
-//alert('hello world');
+jQuery(document).ready(function () {
+    jQuery('.single-product .woocommerce-product-gallery, .single-product .woocommerce-notices-wrapper, .single-product ul.tabs.wc-tabs').hide();   
+        
+    
 
-//woocommerce-MyAccount-downloads-file
+    //Move the add to basket to the right place
+    jQuery('.entry-summary').detach().prependTo('.product-info');
+});
 
-// document.querySelector('.woocommerce-MyAccount-downloads-file').addEventListener('click', (e) => {
-//     window.open(url, "_blank");
-//     console.log('clicked')
-// })
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let para = document.getElementById("clicked-para");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+para.onclick = () => {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), clos	e the modal
+span.onclick = () => {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}	
---
+++ single-product.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<?php if( get_field('parallax_image') ): ?>
+	<div class="parallax" id="clicked-para" style="background-image: url(<?php the_field('parallax_image'); ?>);"></div>
+<?php endif; ?>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+	
+	<div class="embed-container">
+		<?php the_field('video_pop_up'); ?>
+	</div>
+  </div>
+
+</div>
+
+
+
+
+<script>
+
+
+</script>
+
+
+<div class="wrapper" id="single-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<main class="site-main" id="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					
+					<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+						<header class="entry-header">
+
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+							<div class="entry-meta">
+
+								<?php understrap_posted_on(); ?>
+
+							</div><!-- .entry-meta -->
+
+						</header><!-- .entry-header -->
+
+						<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+						<div class="entry-content woocommer-wrapper">
+
+						<div class="product-info-wrap">
+
+						<div class="product-info-images">
+							<img src="<?php the_field('product_image_one'); ?>" />
+							<img src="<?php the_field('product_image_two'); ?>" />
+						</div>
+
+
+						<div class="product-info">
+							<?php
+		
+								if( have_rows('content') ):
+
+										// loop through the rows of data
+										while ( have_rows('content') ) : the_row();
+
+												if( get_row_layout() == 'content_info' ):?>
+
+													<?php the_sub_field('title'); ?>
+													<?php the_sub_field('running_time'); ?>
+													<?php the_sub_field('screen_size'); ?>
+													<?php the_sub_field('subtitles'); ?>
+													<?php the_sub_field('extra_info'); ?>
+
+												<?php 
+
+												endif;
+
+										endwhile;
+
+								else :
+
+										// no layouts found
+
+								endif;
+
+								?>
+
+						</div>
+
+							</div>
+
+							<?php the_content(); ?>
+
+							<?php
+							wp_link_pages(
+								array(
+									'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+									'after'  => '</div>',
+								)
+							);
+							?>
+
+						</div><!-- .entry-content -->
+
+						<footer class="entry-footer">
+
+							<?php understrap_entry_footer(); ?>
+
+						</footer><!-- .entry-footer -->
+
+					</article><!-- #post-## -->
+
+					
+
+					<?php understrap_post_nav(); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+		
+		
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #single-wrapper -->
+
+<?php get_footer(); ?>
